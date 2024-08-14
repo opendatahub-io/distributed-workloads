8ff3d7c224d717b0efb0ceb8b82d7d00268eca51
Antonin Stefanutti <antonin@stefanutti.fr>
2024-08-14 11:57:08 +0200

    Add GSM8k dataset pre-processing for Llama 3

--- examples/ray-finetune-llm-deepspeed/create_dataset.py
+++ examples/ray-finetune-llm-deepspeed/create_dataset.py
@@ -27,5 +27,58 @@ def gsm8k_qa_tokens_template():
                 f.write(json.dumps(i) + "\n")
 
 
+def gsm8k_qa_no_tokens_template():
+    dataset = load_dataset("gsm8k", "main")
+    dataset_splits = {"train": dataset["train"], "test": dataset["test"]}
+
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    with open("data/config.json", "w") as f:
+        config = {
+            "chat_template": (
+                "{% for message in messages %}"
+                "{% if message['role'] == 'system' %}"
+                "{{ message['content'] }}"
+                "{% elif message['role'] == 'user' %}"
+                "{{ '\n\nQuestion: ' + message['content'] +  eos_token }}"
+                "{% elif message['role'] == 'assistant' %}"
+                "{{ '\n\nAnswer: '  + message['content'] +  eos_token  }}"
+                "{% endif %}"
+                "{% endfor %}"
+                "{% if add_generation_prompt %}"
+                "{{ '\n\nAnswer: ' }}"
+                "{% endif %}"
+            )
+        }
+        f.write(json.dumps(config))
+
+    for key, ds in dataset_splits.items():
+        with open(f"data/{key}.jsonl", "w") as f:
+            for item in ds:
+                i = {"messages": [
+                    {"role": "user", "content": item['question']},
+                    {"role": "assistant", "content": item['answer']},
+                ]}
+                f.write(json.dumps(i) + "\n")
+
+
+def gsm8k_hf_chat_template():
+    dataset = load_dataset("gsm8k", "main")
+    dataset_splits = {"train": dataset["train"], "test": dataset["test"]}
+
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    for key, ds in dataset_splits.items():
+        with open(f"data/{key}.jsonl", "w") as f:
+            for item in ds:
+                i = {"messages": [
+                    {"role": "user", "content": item['question']},
+                    {"role": "assistant", "content": item['answer']},
+                ]}
+                f.write(json.dumps(i) + "\n")
+
+
 if __name__ == "__main__":
-    gsm8k_qa_tokens_template()
+    gsm8k_hf_chat_template()
