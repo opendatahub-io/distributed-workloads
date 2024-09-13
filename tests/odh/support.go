38b2fa49bc158738648bb4a344ef285c02c2620a
Karel Suta <ksuta@redhat.com>
2024-09-13 13:50:32 +0200

    Adjust integration tests to updated Ray cluster client

--- tests/odh/support.go
+++ tests/odh/support.go
@@ -44,23 +44,23 @@ func ReadFileExt(t support.Test, fileName string) []byte {
 	return file
 }
 
-func GetDashboardUrl(test support.Test, namespace *v1.Namespace, rayCluster *rayv1.RayCluster) *url.URL {
+func GetDashboardUrl(test support.Test, namespace *v1.Namespace, rayCluster *rayv1.RayCluster) string {
 	dashboardName := "ray-dashboard-" + rayCluster.Name
 	route := support.GetRoute(test, namespace.Name, dashboardName)
 	hostname := route.Status.Ingress[0].Host
 	dashboardUrl, _ := url.Parse("https://" + hostname)
 	test.T().Logf("Ray-dashboard route : %s\n", dashboardUrl.String())
 
-	return dashboardUrl
+	return dashboardUrl.String()
 }
 
-func GetTestJobId(test support.Test, rayClient support.RayClusterClient, hostName string) string {
-	allJobsData, err := rayClient.GetJobs()
+func GetTestJobId(test support.Test, rayClient support.RayClusterClient) string {
+	allJobsData, err := rayClient.ListJobs()
 	test.Expect(err).ToNot(gomega.HaveOccurred())
+	test.Expect(allJobsData).NotTo(gomega.BeEmpty())
 
-	jobID := (*allJobsData)[0].SubmissionID
-	if len(*allJobsData) > 0 {
-		test.T().Logf("Ray job has been successfully submitted to the raycluster with Submission-ID : %s\n", jobID)
-	}
+	jobID := allJobsData[0].SubmissionID
+	test.T().Logf("Ray job has been successfully submitted to the raycluster with Submission-ID : %s\n", jobID)
+
 	return jobID
 }
