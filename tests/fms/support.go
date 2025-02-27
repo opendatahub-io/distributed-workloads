f70b4de9da74f539a30442e6bf8013e8f1c40b17
Karel Suta <ksuta@redhat.com>
2025-02-27 16:22:39 +0100

    Adjust fms-hf-tuning tests to use granite-3b-code-base-2k model

--- tests/fms/support.go
+++ tests/fms/support.go
@@ -48,7 +48,7 @@ func uploadToS3(test Test, namespace string, pvcName string, storedAssetsPath st
 	test.Expect(found).To(BeTrue(), "Storage bucket secret key needs to be specified for S3 upload")
 	bucketName, found := GetStorageBucketName()
 	test.Expect(found).To(BeTrue(), "Storage bucket name needs to be specified for S3 upload")
-	bucketPath := GetStorageBucketModelPath()
+	bucketPath := GetStorageBucketUploadModelPath()
 
 	// Create Secret with AWS/Minio access credentials
 	secretData := map[string]string{
@@ -115,4 +115,82 @@ func uploadToS3(test Test, namespace string, pvcName string, storedAssetsPath st
 		),
 	)
 	test.Expect(GetJob(test, namespace, job.Name)).To(WithTransform(JobConditionCompleted, Equal(corev1.ConditionTrue)), "Job uploading content to S3 bucket failed")
+}
+
+func downloadFromS3(test Test, namespace string, pvcName string, storedAssetsPath string) {
+	defaultEndpoint, found := GetStorageBucketDefaultEndpoint()
+	test.Expect(found).To(BeTrue(), "Storage bucket default endpoint needs to be specified for download from S3")
+	accessKeyId, found := GetStorageBucketAccessKeyId()
+	test.Expect(found).To(BeTrue(), "Storage bucket access key id needs to be specified for download from S3")
+	secretKey, found := GetStorageBucketSecretKey()
+	test.Expect(found).To(BeTrue(), "Storage bucket secret key needs to be specified for download from S3")
+	bucketName, found := GetStorageBucketName()
+	test.Expect(found).To(BeTrue(), "Storage bucket name needs to be specified for download from S3")
+	bucketPath := GetStorageBucketDownloadModelPath()
+
+	// Create Secret with AWS/Minio access credentials
+	secretData := map[string]string{
+		"S3ENDPOINT":  defaultEndpoint,
+		"ACCESSKEYID": accessKeyId,
+		"SECRETKEY":   secretKey,
+	}
+	secret := CreateSecret(test, namespace, secretData)
+	defer test.Client().Core().CoreV1().Secrets(namespace).Delete(test.Ctx(), secret.Name, metav1.DeleteOptions{})
+
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "download-",
+		},
+		Spec: batchv1.JobSpec{
+			BackoffLimit: Ptr(int32(0)),
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					RestartPolicy: corev1.RestartPolicyNever,
+					Containers: []corev1.Container{
+						{
+							Name:    "s3",
+							Image:   GetMinioCliImage(),
+							Command: []string{"/bin/sh", "-c"},
+							Args:    []string{fmt.Sprintf("mc alias set mys3 $S3ENDPOINT $ACCESSKEYID $SECRETKEY; mc cp --recursive mys3/%s/%s /mnt/%s", bucketName, bucketPath, storedAssetsPath)},
+							EnvFrom: []corev1.EnvFromSource{
+								{
+									SecretRef: &corev1.SecretEnvSource{
+										LocalObjectReference: corev1.LocalObjectReference{Name: secret.Name},
+									},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "mounted-volume",
+									MountPath: "/mnt",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "mounted-volume",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: pvcName,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	job, err := test.Client().Core().BatchV1().Jobs(namespace).Create(test.Ctx(), job, metav1.CreateOptions{})
+	test.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Created Job %s/%s successfully", job.Namespace, job.Name)
+	defer test.Client().Core().BatchV1().Jobs(namespace).Delete(test.Ctx(), job.Name, metav1.DeleteOptions{PropagationPolicy: Ptr(metav1.DeletePropagationBackground)})
+
+	test.Eventually(Job(test, namespace, job.Name), 60*time.Minute).Should(
+		Or(
+			WithTransform(JobConditionCompleted, Equal(corev1.ConditionTrue)),
+			WithTransform(JobConditionFailed, Equal(corev1.ConditionTrue)),
+		),
+	)
+	test.Expect(GetJob(test, namespace, job.Name)).To(WithTransform(JobConditionCompleted, Equal(corev1.ConditionTrue)), "Job downloading content from S3 bucket failed")
 }
