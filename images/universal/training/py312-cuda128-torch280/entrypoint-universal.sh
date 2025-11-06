da3c00e3f1a080ca7647759daf31667d6b7a1ab6
Brian Gallagher <briangal@gmail.com>
2025-11-06 14:16:23 +0000

    update entrypoint for universal image
    Signed-off-by: Brian Gallagher <briangal@gmail.com>

--- images/universal/training/py312-cuda128-torch280/entrypoint-universal.sh
+++ images/universal/training/py312-cuda128-torch280/entrypoint-universal.sh
@@ -2,23 +2,23 @@
 set -e
 # Universal entrypoint
 # Behavior:
-# - If RUNTIME_MODE is set to a truthy value, run provided command (headless). If no command provided, exit with help.
-# - Otherwise, start the workbench notebook exactly like the base image.
+# - If NOTEBOOK_ARGS is set, start the workbench notebook with those args (if any).
+# - Otherwise, run the provided command. If no command provided, exit with help.
 
-is_truthy() {
-	case "$(printf %s "$1" | tr '[:upper:]' '[:lower:]')" in
-		y|yes|true|1) return 0 ;;
-		*) return 1 ;;
-	esac
-}
-
-if is_truthy "${RUNTIME_MODE:-}"; then
-	if [ "$#" -gt 0 ]; then
-		exec "$@"
+# If NOTEBOOK_ARGS is present (even if empty), run start-notebook.sh
+if [ -n "${NOTEBOOK_ARGS+x}" ]; then
+	if [ -n "${NOTEBOOK_ARGS}" ]; then
+		# Use a login shell to correctly parse NOTEBOOK_ARGS word splitting and quotes
+		exec sh -lc "exec start-notebook.sh ${NOTEBOOK_ARGS}"
 	else
-		echo "RUNTIME_MODE=true but no command provided. Provide a command, e.g.: python -m your.module" >&2
-		exit 2
+		exec start-notebook.sh
 	fi
 fi
 
-exec start-notebook.sh
+# Otherwise, run provided command, or error if none
+if [ "$#" -gt 0 ]; then
+	exec "$@"
+else
+	echo "No NOTEBOOK_ARGS set and no command provided. Either set NOTEBOOK_ARGS to run start-notebook.sh, or provide a command, e.g.: python -m your.module" >&2
+	exit 2
+fi
