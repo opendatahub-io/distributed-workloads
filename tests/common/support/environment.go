ec92ff3b24fd85866e35a88a8e9110619b2e539f
Fiona Waters <fiwaters6@gmail.com>
2025-12-08 16:47:01 +0000

    Adding SFT E2E Test

--- tests/common/support/environment.go
+++ tests/common/support/environment.go
@@ -58,6 +58,7 @@ const (
 	storageBucketName            = "AWS_STORAGE_BUCKET"
 	storageBucketMnistDir        = "AWS_STORAGE_BUCKET_MNIST_DIR"
 	storageBucketFashionMnistDir = "AWS_STORAGE_BUCKET_FASHION_MNIST_DIR"
+	storageBucketSftDir          = "AWS_STORAGE_BUCKET_SFT_DIR"
 
 	// Name of existing namespace to be used for test
 	testNamespaceNameEnvVar = "TEST_NAMESPACE_NAME"
@@ -183,6 +184,11 @@ func GetStorageBucketMnistDir() (string, bool) {
 func GetStorageBucketFashionMnistDir() (string, bool) {
 	storage_bucket_fashion_mnist_dir, exists := os.LookupEnv(storageBucketFashionMnistDir)
 	return storage_bucket_fashion_mnist_dir, exists
+}
+
+func GetStorageBucketSftDir() (string, bool) {
+	storage_bucket_sft_dir, exists := os.LookupEnv(storageBucketSftDir)
+	return storage_bucket_sft_dir, exists
 }
 
 func GetPipIndexURL() string {
