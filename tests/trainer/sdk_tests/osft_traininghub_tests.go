dea131e950aa2f669981d32e86357886efbbc215
Fiona Waters <fiwaters6@gmail.com>
2025-12-17 09:15:02 +0000

    Add ipykernel package

--- tests/trainer/sdk_tests/osft_traininghub_tests.go
+++ tests/trainer/sdk_tests/osft_traininghub_tests.go
@@ -89,7 +89,7 @@ func RunOsftTrainingHubMultiGpuDistributedTraining(t *testing.T) {
 			"export AWS_SECRET_ACCESS_KEY='%s'; "+
 			"export AWS_STORAGE_BUCKET='%s'; "+
 			"export AWS_STORAGE_BUCKET_OSFT_DIR='%s'; "+
-			"python -m pip install --quiet --no-cache-dir --break-system-packages papermill boto3==1.34.162 && "+
+			"python -m pip install --quiet --no-cache-dir --break-system-packages ipykernel papermill boto3==1.34.162 && "+
 			"if python -m papermill -k python3 /opt/app-root/notebooks/%s /opt/app-root/src/out.ipynb --log-output; "+
 			"then echo 'NOTEBOOK_STATUS: SUCCESS'; else echo 'NOTEBOOK_STATUS: FAILURE'; fi; sleep infinity",
 		support.GetOpenShiftApiUrl(test), userToken, namespace.Name, rwxPvc.Name,
