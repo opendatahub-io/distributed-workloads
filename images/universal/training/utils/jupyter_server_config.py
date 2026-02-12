098026a2ffac6a342932c4f0ffb6683a10f53255
MStokluska <mstoklus@redhat.com>
2026-02-12 14:48:48 +0100

    removal of previous universal image files

--- images/universal/training/utils/jupyter_server_config.py
+++
@@ -1,6 +0,0 @@
-# https://github.com/opendatahub-io-contrib/workbench-images/blob/main/snippets/ides/1-jupyter/files/etc/jupyter_notebook_config.py
-c = get_config()
-# Disable unsupported exporters
-c.WebPDFExporter.enabled = False
-c.QtPDFExporter.enabled = False
-c.QtPNGExporter.enabled = False
